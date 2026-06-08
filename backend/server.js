f561cc6aa45c69d882c4e5dae727a67c122becea
NamaGitHubYou <omar@gmail.com>
2026-06-09 01:52:25 +0800

    TEST: Hardcode Supabase keys to diagnose Render issue

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,9 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// 🔑 Baca dari environment variables
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+// --- Gunakan Hardcode untuk Ujian ---
+const supabaseUrl = "https://mxaanohwaafzshwksqrt.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14YWFub2h3YWFmenNod2tzcXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMjU5MDAsImV4cCI6MjA2NDkwMTkwMH0.ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 if (!supabaseUrl || !supabaseAnonKey) {
     console.error("❌ Missing Supabase environment variables!");
