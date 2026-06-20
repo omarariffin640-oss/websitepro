5d0a95cc537e6f0560c63711810aec2947be6cb2
NamaGitHubYou <omar@gmail.com>
2026-06-20 23:44:02 +0800

    fix: remove onMenuClick prop from Topbar in all pages

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -54,7 +54,7 @@ export default function AccountsPage() {
 
     return (
         <div className="min-h-screen bg-black">
-            <Topbar onMenuClick={() => setSidebarOpen(true)} />
+            <Topbar />
             <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
             <main className="lg:ml-64 pt-2">
